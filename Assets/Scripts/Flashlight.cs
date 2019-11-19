af5cb188813ace654470a919e06b58101cf5fb6c
Christian Vasquez <cvleon@berkeley.edu>
2019-11-18 16:20:46 -0800

    You can now grab pieces

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public AudioClip soundOn;
+    public AudioClip soundOff; 
+    public Light light;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            light.enabled = !light.enabled;
+            audio.clip = audio.clip == soundOn ? soundOff : soundOn;
+            audio.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+    public OVRInput.Controller controller;
+
+    private float indexState = 0;
+    private float handState = 0;
+
+    private bool grabbing = false;
+    private ChessPiece chessPiece = null;
+
+    public void Update() {
+        indexState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+        handState = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
+
+        if (grabbing && indexState <= 0.9f) {
+            Release();
+        }
+    }
+
+    public void OnTriggerStay(Collider other) {
+        Debug.Log(other.gameObject);
+        if (other.CompareTag("ChessPiece")) {
+            Debug.Log(grabbing + " " + indexState);
+            if (!grabbing && indexState > 0.9f) {
+                Grab(other.gameObject);
+            }
+        }
+    }
+
+    private void Grab(GameObject piece) {
+        grabbing = true;
+        chessPiece = piece.GetComponent<ChessPiece>();
+        chessPiece.pickUp(gameObject);
+    }
+
+    private void Release() {
+        grabbing = false;
+        chessPiece.drop();
+        chessPiece = null;
+    }
+}
