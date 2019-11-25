42aea4a0b978ffa9ca4e2090541dd2f879a31ece
Christian <cvleon@berkeley.edu>
2019-11-25 14:30:42 -0800

    Add Basic Hover Trigger

---
+++ Assets/Scripts/BasicTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTrigger : MonoBehaviour
+{
+    //Make object hover until all required objects are present. 
+    public float hoverForce = 12f;
+    private void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * hoverForce, ForceMode.Acceleration);
+        Debug.Log("Trying to add Force");
+    }
+}
