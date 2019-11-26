9827770a952cd8ad85cf585b3e00923d78c032fa
Christian <cvleon@berkeley.edu>
2019-11-25 16:45:13 -0800

    FruitScene with VRTK

--- Assets/Scripts/BasicTrigger.cs
+++ Assets/Scripts/BasicTrigger.cs
@@ -6,9 +6,15 @@ public class BasicTrigger : MonoBehaviour
 {
     //Make object hover until all required objects are present. 
     public float hoverForce = 12f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag == "TriggerKey");
+
+
+    }
     private void OnTriggerStay(Collider other)
     {
         other.GetComponent<Rigidbody>().AddForce(Vector3.up * hoverForce, ForceMode.Acceleration);
-        Debug.Log("Trying to add Force");
     }
 }
